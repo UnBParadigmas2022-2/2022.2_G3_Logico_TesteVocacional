2ff7c2c8fd45872e804b096071cc3783d9576c96
LorrayneCardozo <lorraynecardozo15@gmail.com>
2022-12-29 14:14:44 -0300

    Criando perguntas sobre a área de Engenharia
    Co-authored-by: Ugor <ugor_gost@hotmail.com>

---
+++ code/questions.pl
@@ -0,0 +1,36 @@
+% --------- PERGUNTAS ---------
+% Engenharia
+industrial :- 
+pergunta('Você tem aptidão para desenvolver habilidades físicas e matemáticas?'), 
+pergunta('Você tem alto potencial criativo para desenho e interpretação de processos?'), 
+pergunta('Você tem a capacidade de ser proativo e dinâmico?').
+
+mecanico :- 
+pergunta('Você tem alta habilidade analítica?'),
+pergunta('Você tem a habilidade de fazer representações gráficas?'), 
+pergunta('Você tem habilidade em criar e modificar coisas?').
+
+metalurgico :- 
+pergunta('Você tem capacidade e facilidade para discriminação visual e manipulação de equipamentos e instrumentos?'), 
+pergunta('Você tem facilidade em química, física e matemática?'), 
+pergunta('Você tem habilidades em liderança?').
+
+sistemas :- 
+('Você tem capacidade analítica para avaliar os problemas colocados pela realidade profissional?'), 
+pergunta('Você tem capacidade de liderança para gerenciar sistemas integrados de informação?'), 
+pergunta('Você tem as habilidades organizacionais para estruturar sistemas e suas inter-relações?').
+
+materiais :- 
+pergunta('Você tem a capacidade de abstração, senso lógico e habilidade em lidar com atividades abstratas?'), 
+pergunta('Você tem facilidade em química, física e matemática?'), 
+pergunta('Você tem facilidade em aprender?').
+
+minas :- 
+pergunta('Você tem habilidades e vocação para liderança?'), 
+pergunta('Você tem boas condições físicas?'), 
+pergunta('Você tem capacidade de subir montanhas?').
+
+mecatronico :-
+pergunta('Você tem grande curiosidade e capacidade em procurar saber como as coisas fucionam?'), 
+pergunta('Você tem grande capacidade para criar coisas?'), 
+pergunta('Você tem habilidade em criar soluções tecnológicas?').
