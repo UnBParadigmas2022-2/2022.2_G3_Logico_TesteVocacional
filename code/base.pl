20907ba492a443afba44a67c08e5ab431b628a49
Gustave Persijn <gpersijn@outlook.com>
2023-01-03 21:20:48 -0300

    Adicionando base de conhecimento.
    Co-authored-by: Ugor <ugor_gost@hotmail.com>
    Co-authored-by: savioc2 <saviocunha61@gmail.com>

--- code/base.pl
+++ code/base.pl
@@ -1,47 +1,175 @@
 % --------- BASE DE CONHECIMENTO ---------
-% Engenharia
-carreira('Engenheiro Industrial') :- industrial,!.
-carreira('Engenheiro Mecânico') :- mecanico,!.
-carreira('Engenheiro Metalúrgico') :- metalurgico,!.
-carreira('Engenheiro de Sistemas') :- sistemas,!.
-carreira('Engenheiro de Materiais') :- materiais,!.
-carreira('Engenheiro de Minas') :- minas,!.
-carreira('Engenheiro Mecatrônico') :- mecatronico,!.
-carreira('Engenheiro Quimica') :- quimica,!.
+carreira('Psicólogo').
+carreira('Arquiteto').       
+carreira('Dançarino').      
+carreira('Artista plástico').     
+carreira('Personal trainer').
+carreira('Designer').  
+carreira('Atleta'). 
+carreira('Professor'). 
+carreira('Policial'). 
+carreira('Bombeiro'). 
+carreira('Engenheiro Industrial').
+carreira('Engenheiro Mecânico'). 
+carreira('Engenheiro Metalúrgico').
+carreira('Engenheiro de Software'). 
+carreira('Engenheiro de Materiais'). 
+carreira('Engenheiro de Minas'). 
+carreira('Engenheiro Mecatrônico'). 
+carreira('Engenheiro Quimica').
+carreira('Licenciado em Administração'). 
+carreira('Contador'). 
+carreira('Economista').
+carreira('Biologo').
+carreira('Biologo Microbiologo').
+carreira('Biologo Marinho').
+carreira('Engenheiro Agricola').
+carreira('Engenheiro Zootecnista').
+carreira('Engenheiro Agronomo').
+carreira('Advogado').
+carreira('Cientista Politico').
+carreira('Enfermeiro').
+carreira('Cientista Social').
+carreira('Medico').
+carreira('Farmaceutico').
+carreira('Fisico').
+carreira('Matematico').
+carreira('Ciencias da Comunicação').
 
-% Ciencias Economicas
-carreira('Licenciado em Administração') :- administracao,!.
-carreira('Contador') :- contador,!.
-carreira('Economista') :- economista,!.
+% --------- PRECISA DE INGLÊS ---------
+anglofono('Engenheiro Industrial').
+anglofono('Engenheiro Mecânico').
+anglofono('Engenheiro Metalúrgico').
+anglofono('Engenheiro de Software').
+anglofono('Engenheiro de Materiais').
+anglofono('Engenheiro de Minas').
+anglofono('Engenheiro Mecatrônico').
+anglofono('Engenheiro Quimica').
+anglofono('Economista').
+anglofono('Engenheiro Agricola').
+anglofono('Engenheiro Zootecnista').
+anglofono('Engenheiro Agronomo').
+anglofono('Cientista Politico').
+anglofono('Cientista Social').
+anglofono('Ciencias da Comunicação').
+anglofono('Arquiteto').
+anglofono('Designer').
 
-% Ciencias Biologicas
-carreira('Biologo'):-biologo,!.
-carreira('Biologo Microbiologo'):-microbiologo,!.
-carreira('Biologo Marinho'):-marinho,!
+% --------- FACILIDADE COM CÁLCULOS ---------
+calculo('Engenheiro Industrial').
+calculo('Engenheiro Mecânico').
+calculo('Engenheiro Metalúrgico').
+calculo('Engenheiro de Software').
+calculo('Engenheiro de Materiais').
+calculo('Engenheiro de Minas').
+calculo('Engenheiro Mecatrônico').
+calculo('Engenheiro Quimica').
+calculo('Economista').
+calculo('Engenheiro Agricola').
+calculo('Engenheiro Zootecnista').
+calculo('Engenheiro Agronomo').
+calculo('Contador').
+calculo('Farmaceutico').
+calculo('Fisico').
+calculo('Matematico').
+calculo('Arquiteto').
 
-% Ciências Agropecuárias
-carreira('Engenheiro Agricola'):-agricola,!.
-carreira('Engenheiro Zootecnista'):-zootecnista,!.
-carreira('Engenheiro Agronomo'):- agronomo,!.
+% --------- COMUNICAÇÃO ---------
+comunicacao('Licenciado em Administração').
+comunicacao('Engenheiro de Software').
+comunicacao('Advogado').
+comunicacao('Cientista Politico').
+comunicacao('Enfermeiro').
+comunicacao('Cientista Social').
+comunicacao('Medico').
+comunicacao('Farmaceutico').
+comunicacao('Ciencias da Comunicação').
+comunicacao('Arquiteto').       
+comunicacao('Dançarino').
+comunicacao('Personal trainer').
+comunicacao('Designer').
+comunicacao('Atleta').
+comunicacao('Professor').
+comunicacao('Policial'). 
+comunicacao('Bombeiro').
+comunicacao('Psicólogo').
 
-% Direito e Ciencias Politicas
-carreira('Advogado'):- advogado,!.
-carreira('Cientista Politico'):- politico,!.
+% --------- CRIATIVIDADE ---------
+criatividade('Arquiteto').
+criatividade('Dançarino').
+criatividade('Artista plástico').
+criatividade('Designer').
+criatividade('Atleta').
+criatividade('Professor').
 
-% Enfermagem
-carreira('Enfermeiro'):-enfermeiro,!.
+% --------- APTIDÃO FÍSICA ---------
+aptidao('Dançarino').
+aptidao('Atleta').
+aptidao('Personal trainer').
+aptidao('Policial').
+aptidao('Bombeiro').
 
-% Ciencias Humanas
-carreira('Cientista Social'):- sociais,!.
+% ----------- LEITURA --------------
+leitura('Arquiteto').       
+leitura('Professor').
+leitura('Engenheiro Industrial').
+leitura('Engenheiro de Materiais').
+leitura('Engenheiro Quimica').
+leitura('Licenciado em Administração').
+leitura('Economista').
+leitura('Biologo').
+leitura('Biologo Microbiologo').
+leitura('Advogado').
+leitura('Cientista Politico').
+leitura('Cientista Social').
+leitura('Ciencias da Comunicação').
+leitura('Psicólogo').
 
-% Saude
-carreira('Medico'):- medico,!.
-carreira('Farmaceutico'):- farmaceutico,!.
+% ----------- EMPATIA EM AJUDAR PESSOAS -------------- 
+empatia('Psicólogo').
+empatia('Personal trainer').
+empatia('Professor'). 
+empatia('Policial'). 
+empatia('Bombeiro'). 
+empatia('Enfermeiro').
+empatia('Medico').
 
-% Ciencias Fisicas E Matematicas
-carreira('Fisico'):- fisico,!.
-carreira('Matematico'):- matematico,!.
+% ----------- TRABALHAR EM LOCAIS FECHADOS -------------- 
+locais_fechados('Psicólogo').
+locais_fechados('Arquiteto').
+locais_fechados('Artista plástico').
+locais_fechados('Designer').
+locais_fechados('Professor').
+locais_fechados('Engenheiro Industrial').
+locais_fechados('Engenheiro Mecânico'). 
+locais_fechados('Engenheiro Metalúrgico').
+locais_fechados('Engenheiro de Software'). 
+locais_fechados('Engenheiro de Materiais'). 
+locais_fechados('Engenheiro de Minas'). 
+locais_fechados('Engenheiro Mecatrônico'). 
+locais_fechados('Engenheiro Quimica').
+locais_fechados('Licenciado em Administração'). 
+locais_fechados('Contador'). 
+locais_fechados('Economista').
+locais_fechados('Biologo').
+locais_fechados('Biologo Microbiologo').
+locais_fechados('Advogado').
+locais_fechados('Cientista Politico').
+locais_fechados('Enfermeiro').
+locais_fechados('Cientista Social').
+locais_fechados('Medico').
+locais_fechados('Farmaceutico').
+locais_fechados('Fisico').
+locais_fechados('Matematico').
+locais_fechados('Ciencias da Comunicação').
 
-% Educação e Ciencias Da comunicação
-carreira('Ciencias da Comunicação'):- ccomunicacao,!.
-carreira('Educação'):- educacao,!.
+% ----------- TECNOLOGIA -------------- 
+tecnologia('Arquiteto').
+tecnologia('Designer').
+tecnologia('Atleta').
+tecnologia('Policial'). 
+tecnologia('Bombeiro').
+tecnologia('Engenheiro de Software').
+tecnologia('Biologo Marinho').
+tecnologia('Enfermeiro').
+tecnologia('Medico').
