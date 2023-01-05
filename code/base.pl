cbad913793951da3fd0f8aa6e9fb640e3b6ca21d
Ugor Marcilio Brandão Costa <52638444+ubrando@users.noreply.github.com>
2023-01-04 22:03:38 -0300

    Atualizando a base de conhecimento
    Mudando a lógica da base para ser usada na Main além de adequar o número de restrições ao escopo do projeto
    
    Co-Authored-By: Lorrayne Cardozo <lorraynecardozo15@gmail.com>
    Co-Authored-By: Lorrany Oliveira  <lolo.souza80@yahoo.com>
    Co-Authored-By: Eduarda Servidio Claudino <52542729+ServidioEC@users.noreply.github.com>

--- code/base.pl
+++ code/base.pl
@@ -1,175 +1,40 @@
 % --------- BASE DE CONHECIMENTO ---------
-carreira('Psicólogo').
-carreira('Arquiteto').       
-carreira('Dançarino').      
-carreira('Artista plástico').     
-carreira('Personal trainer').
-carreira('Designer').  
-carreira('Atleta'). 
-carreira('Professor'). 
-carreira('Policial'). 
-carreira('Bombeiro'). 
-carreira('Engenheiro Industrial').
-carreira('Engenheiro Mecânico'). 
-carreira('Engenheiro Metalúrgico').
-carreira('Engenheiro de Software'). 
-carreira('Engenheiro de Materiais'). 
-carreira('Engenheiro de Minas'). 
-carreira('Engenheiro Mecatrônico'). 
-carreira('Engenheiro Quimica').
-carreira('Licenciado em Administração'). 
-carreira('Contador'). 
-carreira('Economista').
-carreira('Biologo').
-carreira('Biologo Microbiologo').
-carreira('Biologo Marinho').
-carreira('Engenheiro Agricola').
-carreira('Engenheiro Zootecnista').
-carreira('Engenheiro Agronomo').
-carreira('Advogado').
-carreira('Cientista Politico').
-carreira('Enfermeiro').
-carreira('Cientista Social').
-carreira('Medico').
-carreira('Farmaceutico').
-carreira('Fisico').
-carreira('Matematico').
-carreira('Ciencias da Comunicação').
 
-% --------- PRECISA DE INGLÊS ---------
-anglofono('Engenheiro Industrial').
-anglofono('Engenheiro Mecânico').
-anglofono('Engenheiro Metalúrgico').
-anglofono('Engenheiro de Software').
-anglofono('Engenheiro de Materiais').
-anglofono('Engenheiro de Minas').
-anglofono('Engenheiro Mecatrônico').
-anglofono('Engenheiro Quimica').
-anglofono('Economista').
-anglofono('Engenheiro Agricola').
-anglofono('Engenheiro Zootecnista').
-anglofono('Engenheiro Agronomo').
-anglofono('Cientista Politico').
-anglofono('Cientista Social').
-anglofono('Ciencias da Comunicação').
-anglofono('Arquiteto').
-anglofono('Designer').
-
-% --------- FACILIDADE COM CÁLCULOS ---------
-calculo('Engenheiro Industrial').
-calculo('Engenheiro Mecânico').
-calculo('Engenheiro Metalúrgico').
-calculo('Engenheiro de Software').
-calculo('Engenheiro de Materiais').
-calculo('Engenheiro de Minas').
-calculo('Engenheiro Mecatrônico').
-calculo('Engenheiro Quimica').
-calculo('Economista').
-calculo('Engenheiro Agricola').
-calculo('Engenheiro Zootecnista').
-calculo('Engenheiro Agronomo').
-calculo('Contador').
-calculo('Farmaceutico').
-calculo('Fisico').
-calculo('Matematico').
-calculo('Arquiteto').
-
-% --------- COMUNICAÇÃO ---------
-comunicacao('Licenciado em Administração').
-comunicacao('Engenheiro de Software').
-comunicacao('Advogado').
-comunicacao('Cientista Politico').
-comunicacao('Enfermeiro').
-comunicacao('Cientista Social').
-comunicacao('Medico').
-comunicacao('Farmaceutico').
-comunicacao('Ciencias da Comunicação').
-comunicacao('Arquiteto').       
-comunicacao('Dançarino').
-comunicacao('Personal trainer').
-comunicacao('Designer').
-comunicacao('Atleta').
-comunicacao('Professor').
-comunicacao('Policial'). 
-comunicacao('Bombeiro').
-comunicacao('Psicólogo').
-
-% --------- CRIATIVIDADE ---------
-criatividade('Arquiteto').
-criatividade('Dançarino').
-criatividade('Artista plástico').
-criatividade('Designer').
-criatividade('Atleta').
-criatividade('Professor').
-
-% --------- APTIDÃO FÍSICA ---------
-aptidao('Dançarino').
-aptidao('Atleta').
-aptidao('Personal trainer').
-aptidao('Policial').
-aptidao('Bombeiro').
-
-% ----------- LEITURA --------------
-leitura('Arquiteto').       
-leitura('Professor').
-leitura('Engenheiro Industrial').
-leitura('Engenheiro de Materiais').
-leitura('Engenheiro Quimica').
-leitura('Licenciado em Administração').
-leitura('Economista').
-leitura('Biologo').
-leitura('Biologo Microbiologo').
-leitura('Advogado').
-leitura('Cientista Politico').
-leitura('Cientista Social').
-leitura('Ciencias da Comunicação').
-leitura('Psicólogo').
-
-% ----------- EMPATIA EM AJUDAR PESSOAS -------------- 
-empatia('Psicólogo').
-empatia('Personal trainer').
-empatia('Professor'). 
-empatia('Policial'). 
-empatia('Bombeiro'). 
-empatia('Enfermeiro').
-empatia('Medico').
-
-% ----------- TRABALHAR EM LOCAIS FECHADOS -------------- 
-locais_fechados('Psicólogo').
-locais_fechados('Arquiteto').
-locais_fechados('Artista plástico').
-locais_fechados('Designer').
-locais_fechados('Professor').
-locais_fechados('Engenheiro Industrial').
-locais_fechados('Engenheiro Mecânico'). 
-locais_fechados('Engenheiro Metalúrgico').
-locais_fechados('Engenheiro de Software'). 
-locais_fechados('Engenheiro de Materiais'). 
-locais_fechados('Engenheiro de Minas'). 
-locais_fechados('Engenheiro Mecatrônico'). 
-locais_fechados('Engenheiro Quimica').
-locais_fechados('Licenciado em Administração'). 
-locais_fechados('Contador'). 
-locais_fechados('Economista').
-locais_fechados('Biologo').
-locais_fechados('Biologo Microbiologo').
-locais_fechados('Advogado').
-locais_fechados('Cientista Politico').
-locais_fechados('Enfermeiro').
-locais_fechados('Cientista Social').
-locais_fechados('Medico').
-locais_fechados('Farmaceutico').
-locais_fechados('Fisico').
-locais_fechados('Matematico').
-locais_fechados('Ciencias da Comunicação').
-
-% ----------- TECNOLOGIA -------------- 
-tecnologia('Arquiteto').
-tecnologia('Designer').
-tecnologia('Atleta').
-tecnologia('Policial'). 
-tecnologia('Bombeiro').
-tecnologia('Engenheiro de Software').
-tecnologia('Biologo Marinho').
-tecnologia('Enfermeiro').
-tecnologia('Medico').
+%% Carreiras ( nome, calculo, comunicacao, aptidao fisica, leitura, empatia)
+carreira('Psicólogo'					,não	,sim	,não	,sim	,sim).
+carreira('Arquiteto'					,sim	,sim	,sim	,sim	,sim).       
+carreira('Dançarino'					,não	,sim	,sim,	,não	,não).      
+carreira('Artista plástico'				,não	,não	,não	,não	,não).     
+carreira('Personal trainer'				,não	,sim	,sim	,não	,sim).
+carreira('Designer'						,não	,sim	,não	,não	,não).  
+carreira('Atleta'						,não	,sim	,sim	,não	,não). 
+carreira('Professor'					,sim	,sim	,sim	,sim	,sim). 
+carreira('Policial'						,não	,sim	,não	,não	,sim). 
+carreira('Bombeiro'						,não	,sim	,sim	,não	,sim). 
+carreira('Engenheiro Industrial'		,sim	,não	,não	,sim	,sim).
+carreira('Engenheiro Mecânico'			,sim	,não	,não	,não	,não). 
+carreira('Engenheiro Metalúrgico'		,sim	,não	,não	,não	,não).
+carreira('Engenheiro de Software'		,sim	,sim	,não	,não	,não). 
+carreira('Engenheiro de Materiais'		,sim	,não	,não	,sim	,não). 
+carreira('Engenheiro de Minas'			,sim	,não	,não	,não	,não). 
+carreira('Engenheiro Mecatrônico'		,sim	,não	,não	,não	,não). 
+carreira('Engenheiro Quimica'			,sim	,não	,não	,sim	,não).
+carreira('Licenciado em Administração'	,não	,sim	,não	,sim	,não). 
+carreira('Contador'						,não	,não	,não	,não	,não). 
+carreira('Economista'					,sim	,não	,não	,sim	,não).
+carreira('Biologo'						,não	,não	,não	,sim	,não).
+carreira('Biologo Microbiologo'			,não	,não	,não	,sim	,não).
+carreira('Biologo Marinho'				,não	,não	,não	,não	,não).
+carreira('Engenheiro Agricola'			,sim	,não	,não	,não	,não).
+carreira('Engenheiro Zootecnista'		,sim	,não	,não	,não	,não).
+carreira('Engenheiro Agronomo'			,sim	,não	,não	,não	,não).
+carreira('Advogado'						,não	,sim	,não	,sim	,não).
+carreira('Cientista Politico'			,não	,sim	,não	,sim	,não).
+carreira('Enfermeiro'					,não	,sim	,não	,não	,sim).
+carreira('Cientista Social'				,não	,sim	,não	,sim	,não).
+carreira('Medico'						,não	,sim	,não	,não	,sim).
+carreira('Farmaceutico'					,sim	,sim	,não	,não	,não).
+carreira('Fisico'						,sim	,não	,não	,não	,não).
+carreira('Matematico'					,sim	,não	,não	,não	,não).
+carreira('Cientista da Comunicação'		,não	,sim	,não	,sim	,não).
+carreira('Fisioterapeuta'				,sim	,sim	,sim	,sim	,sim).
